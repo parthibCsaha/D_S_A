06bb73ed7c62a0350a46b3a0e544e5a1cd7f4a52
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2023-08-19 23:39:42 +0600

    Add files via upload

---
+++ UnionFind .cpp
@@ -0,0 +1,18 @@
+class UnionFind {
+public: 
+    vector<int> parent; 
+    UnionFind(int n){
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i] = i; 
+    }
+    
+    int findParent(int p) {
+        return parent[p] == p ? p : parent[p] = findParent(parent[p]); 
+    }
+    
+    void Union(int u , int v) {
+        int pu = findParent(u) , pv = findParent(v); 
+        parent[pu] = pv;
+    }    
+};
