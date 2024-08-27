2e69d3fcda97e2b53fc18e75c6fa1a04039252d4
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2024-08-28 00:15:30 +0600

    Update Segment_Tree(Lazy Propagation).cpp

--- Segment_Tree(Lazy Propagation).cpp
+++ Segment_Tree(Lazy Propagation).cpp
@@ -8,42 +8,41 @@ public:
         tree.resize(4*n, 0);
         lazytree.resize(4*n, 0);
     }
-    void rangeupdate(ll l1, ll r1, ll val, ll l, ll r, ll v) {
+    void rangeupdate(ll low, ll high, ll val, ll l, ll r, ll v) {
         if(lazytree[v] != 0) {
-            tree[v] += (r1 - l1 + 1) * lazytree[v];
-            if(l1 != r1) {
+            tree[v] += (high - low + 1) * lazytree[v];
+            if(low != high) {
                 lazytree[2*v] += lazytree[v];
                 lazytree[2*v+1] += lazytree[v];
             }
             lazytree[v] = 0;
         }
-        if(r < l1 or l > r1) return;
-        if(l1 >= l and r1 <= r) {
-            tree[v] += (r1 - l1 + 1) * val;
-            if(l1 != r1) {
-                lazytree[2*v] += lazytree[v];
-                lazytree[2*v+1] += lazytree[v];
+        if(r < low or l > high) return;
+        if(low >= l and high <= r) {
+            tree[v] += (high - low + 1) * val;
+            if(low != high) {
+                lazytree[2*v] += val;
+                lazytree[2*v+1] += val;
             }
             return;
         }
-        ll m = (l1 + r1) / 2;
-        rangeupdate(l1, m, val, l, r, 2*v);
-        rangeupdate(m + 1, r1, val, l, r, 2*v + 1);
-        tree[v] = max(tree[2*v], tree[2*v + 1]);
+        ll m = (low + high) / 2;
+        rangeupdate(low, m, val, l, r, 2*v);
+        rangeupdate(m + 1, high, val, l, r, 2*v + 1);
+        tree[v] = tree[2*v] + tree[2*v + 1];
     }
-    ll query(ll l1, ll r1, ll l, ll r, ll v) {
+    ll query(ll low, ll high, ll l, ll r, ll v) {
         if(lazytree[v] != 0) {
-            tree[v] += (r1 - l1 + 1) * lazytree[v];
-            if(l1 != r1) {
+            tree[v] += (high - low + 1) * lazytree[v];
+            if(low != high) {
                 lazytree[2*v] += lazytree[v];
                 lazytree[2*v+1] += lazytree[v];
             }
             lazytree[v] = 0;
         }
-        if(r < l1 or l > r1) return 0;
-        if(l1 >= l and r1 <= r) return tree[v];
-        ll m = (l + r) / 2;
-        ll ans = query(l1, m, l, r, 2*v)+query(m + 1, r, l, r, 2*v + 1);
-        return ans;
+        if(r < low or l > high) return 0;
+        if(low >= l and high <= r) return tree[v];
+        ll m = (low + high) / 2;
+        return query(low, m, l, r, 2*v) + query(m + 1, high, l, r, 2*v + 1);
     }
 };
