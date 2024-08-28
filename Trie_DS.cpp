4be6a8879aab02e8309bd1a4b8451d54e357be09
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2024-08-28 23:49:39 +0600

    Update Trie_DS.cpp

--- Trie_DS.cpp
+++ Trie_DS.cpp
@@ -1,28 +1,40 @@
-// max xor subarray
 class Triee {
 public:
     ll N,cnt;
+    vector<ll> freq;
     vector<vector<ll>> trie;
     Triee(ll n) {
         N = n;
-        cnt = 0;
+        cnt = 1;
+        freq.resize(N*32, 0);
         trie.resize(N*32, vector<ll>(2,0));
     }
     void insert(ll x) {
-        ll node = 0;
+        ll node = 1;
+        freq[node] ++;
         for(ll i=31;i>=0;i--) {
             ll bit = min(1ll,(1ll<<i)&x);
             if(!trie[node][bit]) {
                 trie[node][bit] = ++cnt;
             }
-            node = trie[node][bit]; 
+            node = trie[node][bit];
+            freq[node] ++; 
+        }
+    }
+    void delett(ll x) {
+        ll node = 1;
+        freq[node] --;
+        for(ll i=31;i>=0;i--) {
+            ll bit = min(1ll,(1ll<<i)&x);
+            node = trie[node][bit];
+            freq[node] --; 
         }
     }
     ll search(ll x) {
-        ll node = 0,ans = 0;
+        ll node = 1,ans = 0;
         for(ll i=31;i>=0;i--) {
             ll bit = min(1ll,(1ll<<i)&x);
-            if(trie[node][bit^1]) {
+            if(freq[trie[node][bit^1]]) {
                 ans |= 1ll<<i;
                 node = trie[node][bit^1];
             }
