ec126457a4a15988786511b2b79458a27ce66daf
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2024-09-26 09:09:38 +0600

    Update Trie_DS.cpp

--- Trie_DS.cpp
+++ Trie_DS.cpp
@@ -45,3 +45,44 @@ public:
         return ans;
     }
 };
+
+// --------------------------------------------------------
+
+struct TrieNode {
+    TrieNode *next[26] = {};
+    int cnt = 0;
+};
+class Solution {
+    TrieNode root;
+public:
+    void insert(string word) {
+        auto node = &root;
+        for(char c : word) {
+            if(!node->next[c - 'a']) {
+                node->next[c - 'a'] = new TrieNode();
+            }
+            node->next[c - 'a']->cnt++;
+            node = node->next[c - 'a'];
+        }
+    }
+    int search(string word) {
+        auto node = &root;
+        int ans = 0;
+        for(char c : word) {
+            ans += node->next[c - 'a']->cnt;
+            node = node->next[c - 'a'];
+        }
+        return ans;
+    }
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int N = words.size();
+        for (int i = 0; i < N; i++) {
+            insert(words[i]);
+        }
+        vector<int> ans(N, 0);
+        for (int i = 0; i < N; i++) {
+            ans[i] = search(words[i]); 
+        }
+        return ans;
+    }
+};
