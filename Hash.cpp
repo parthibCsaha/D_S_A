4b6e462ffd3a432514daf436caad83a5563639d6
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2024-10-22 18:13:46 +0600

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -1,7 +1,22 @@
-        map<ll,ll> h,p;
-        ll sm = 0,k = 19937;
-        p[0] = 1;
-        for(ll i=1;i<=n;i++) {
-            p[i] = (p[i-1] * k) % mod;
-            h[i] = (h[i-1] + (s[i-1]-'a'+1) * p[i]) % mod;
-        }
+class HashedString {
+  private:
+	// change M and B if you want
+	static const long long M = 1e9 + 9;
+	static const long long B = 9973;
+	// pow[i] contains B^i % M
+	static vector<long long> pow;
+	// p_hash[i] is the hash of the first i characters of the given string
+	vector<long long> p_hash;
+  public:
+	HashedString(const string &s) : p_hash(s.size() + 1) {
+		while (pow.size() <= s.size()) { pow.push_back((pow.back() * B) % M); }
+		p_hash[0] = 0;
+		for (int i = 0; i < s.size(); i++) {
+			p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
+		}
+	}
+	long long get_hash(int start, int end) {
+		long long raw_val = (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
+		return (raw_val % M + M) % M;
+	}
+};
