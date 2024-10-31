8209f45254b163fe333d587598ae3f0aff870361
parthibCsaha <105928504+parthibCsaha@users.noreply.github.com>
2024-10-31 09:07:01 +0600

    Update Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -1,26 +1,25 @@
 class SegmenTree {
 public: 
-    ll N;
-    vector<ll> tree;
-    SegmenTree(ll n) {
+    int N;
+    vector<int> tree;
+    SegmenTree(int n) {
         N = n;
         tree.resize(4*n, 0);
     }
-    void update(ll idx, ll val, ll l, ll r, ll v) {
+    void update(int idx, int val, int l, int r, int v) {
         if(l == r) {
             tree[v] = val;
             return;
         }
-        ll m = (l + r) / 2;
+        int m = (l + r) / 2;
         if(idx <= m) update(idx, val, l, m, 2*v);
         else update(idx, val, m + 1, r, 2*v + 1);
         tree[v] = max(tree[2*v], tree[2*v + 1]);
     }
-    ll query(ll x, ll y, ll l, ll r, ll v) {
+    int query(int x, int y, int l, int r, int v) {
         if(r < x or l > y) return 0;
         if(l >= x and r <= y) return tree[v];
-        ll m = (l + r) / 2;
-        ll ans = max(query(x, y, l, m, 2*v),query(x, y, m + 1, r, 2*v + 1));
-        return ans;
+        int m = (l + r) / 2;
+        return query(x, y, l, m, 2*v) + query(x, y, m + 1, r, 2*v + 1);
     }
 };
