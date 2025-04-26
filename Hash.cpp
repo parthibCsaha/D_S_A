911b13a6926255825bbede64fd4a66fa9bace8c0
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-04-27 01:26:43 +0600

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -1,25 +1,26 @@
 mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
-const ll B = uniform_int_distribution<ll>(0, M - 1)(rng);
+const long long B = uniform_int_distribution<long long>(277, 2e18)(rng);
 
 class HashedString {
-  private:
-	// change M and B if you want
-	static const long long M = 1e9 + 9;
-	static const long long B = 9973;
-	// pow[i] contains B^i % M
-	static vector<long long> pow;
-	// p_hash[i] is the hash of the first i characters of the given string
-	vector<long long> p_hash;
-  public:
-	HashedString(const string &s) : p_hash(s.size() + 1) {
-		while (pow.size() <= s.size()) { pow.push_back((pow.back() * B) % M); }
-		p_hash[0] = 0;
-		for (int i = 0; i < s.size(); i++) {
-			p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
-		}
-	}
-	long long get_hash(int start, int end) {
-		long long raw_val = (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
-		return (raw_val % M + M) % M;
-	}
+private:
+    long long M = 1e9 + 9;
+    // long long B = 9973;
+    vector<long long> pow;
+    vector<long long> p_hash;
+public:
+    HashedString(string s) {
+        p_hash.resize(s.size() + 1);
+        pow.push_back(1);
+        while (pow.size() <= s.size()) { 
+            pow.push_back((1LL * pow.back() * (B % M)) % M); 
+        }
+        p_hash[0] = 0;
+        for (int i = 0; i < s.size(); i++) {
+            p_hash[i + 1] = ((p_hash[i] * (B % M)) % M + s[i]) % M;
+        }
+    }
+    long long get_hash(int start, int end) {
+        long long raw_val = (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
+        return (raw_val % M + M) % M;
+    }
 };
